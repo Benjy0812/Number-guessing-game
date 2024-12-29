ba3e4c28d242122ec61a0e0df667731e38f655f8
Benjy <108727379+Benjy0812@users.noreply.github.com>
2024-12-29 21:21:42 +0100

    First commit

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import random
 import os
 
+
 def clear():
-    
+
     if os.name == 'nt':
         os.system('cls')
 
+
 while True:
-    
-    print("This program outputs a random number between 1 and 6")
-    
-    print("Guess a number between 1 and 6")
+
+    print("This program outputs a random number between 1 and 10")
+    print("Guess a number between 1 and 10")
 
     guess = int(input("Write you guess here: "))
 
-    rand = random.randint(1, 6)
+    rand = random.randint(1, 10)
 
     if guess == rand:
-        print(f"You guessed right it was {rand}")      
-    else: 
+        print(f"You guessed right it was {rand}")
+    else:
         print(f"Wrong the number was {rand}")
-        
+
     print("Do you wish to continue? Y/N")
     user_continue = input("Enter Here: ").upper()
 
